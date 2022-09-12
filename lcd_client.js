d70b90fa6959ab4e6f9d860138808dc10cf49ccf
Ádám Lőrincz <adam7605@easv365.dk>
2022-09-12 17:22:45 +0200

    Removed comment

--- lcd_client.js
+++ lcd_client.js
@@ -77,7 +77,7 @@ function getStatus() {
         // called when the complete response is received.
         res.on('end', () => {
             status = JSON.parse(data);
-            console.log("Status received: " + data);
+            //console.log("Status received: " + data);
         });
 
         }).on("error", (err) => {
